b8333aee719a1a93c21d94d9c3bfb0386efbab59
VLola <valiklolanew@gmail.com>
2023-07-27 22:45:38 +0300

    added logout

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,24 @@
 import Link from 'next/link'
 import { FC } from 'react'
 import { AiOutlineSetting } from 'react-icons/ai'
-import { FiLogIn } from 'react-icons/fi'
+import { FiLogIn, FiLogOut } from 'react-icons/fi'
 import styles from './header.module.scss'
 import { NavigationAvatarButton } from './navigation/navigationAvatarButton'
+import { signOut, useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+
+async function logOut(){
+  signOut();
+}
 
 export const Header: FC = () => {
+  
+  const router = useRouter();
+
+  const { data: session } = useSession();
+  if (session === null) {
+    router.push("/signinPage");
+  }
 
   return (
     <>
@@ -14,7 +27,7 @@ export const Header: FC = () => {
           <div className={styles.contentContainer}>
             <Link className={styles.navButton} href='/settingsPage'><AiOutlineSetting size={20} /></Link>
             <Link className={styles.navAvatarButton} href='/profilePage'><NavigationAvatarButton /></Link>
-            <Link className={styles.navButton} href='/signinPage'><FiLogIn size={20} /></Link>
+            {session?.user? (<button className={styles.navButton} onClick={logOut}><FiLogOut size={20} /></button>):(<Link className={styles.navButton} href='/signinPage'><FiLogIn size={20} /></Link>)}
           </div>
         </div>
       </header>
