22f1f00ecd0b0ddbf5f83a5fc445a52c254a5a34
Tibilex <45212175+Tibilex@users.noreply.github.com>
2023-07-27 23:06:43 +0300

    FixedL header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,11 @@
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { FC } from 'react'
 import { AiOutlineSetting } from 'react-icons/ai'
 import { FiLogIn } from 'react-icons/fi'
+import { DropMenuProfile } from '../userProfile/dropMenu/dropMenu'
 import styles from './header.module.scss'
-import { NavigationAvatar } from './navigation/navigationAvatar'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
 
 export const Header: FC = () => {
 
@@ -23,8 +23,8 @@ export const Header: FC = () => {
           <div className={styles.contentContainer}>
             <Link className={styles.navText} href='/profilePage'>Мой профиль</Link>
             <Link className={styles.navButton} href='/settingsPage'><AiOutlineSetting size={20} /></Link>
-            <Link className={styles.navAvatarButton} href='/profilePage'><NavigationAvatar /></Link>
             <Link className={styles.navButton} href='/signinPage'><FiLogIn size={20} /></Link>
+            <DropMenuProfile />
           </div>
         </div>
       </header>
