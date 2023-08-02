2b7025b82af1b37da1991938d4a8d58ecc2be94c
loboda.rom@gmail.com <loboda.rom@gmail.com>
2023-08-02 17:58:39 +0300

    joinGroup,countUsersInGroup

--- src/components/userProfile/groupsCard/card.tsx
+++ src/components/userProfile/groupsCard/card.tsx
@@ -3,11 +3,24 @@ import Image from 'next/image';
 import Link from 'next/link';
 import {AiOutlineUsergroupAdd } from 'react-icons/ai';
 import styles from './card.module.scss';
+import { useSession } from "next-auth/react";
 
 export const Card = (props:any) => {
-
-
-
+  const { data: session, update } = useSession();
+  let JoinGroup = async()=>{
+    let response = await fetch("http://localhost:5288/User/JoinGroup?id="+props.group.id, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        'Content-Type': 'application/json',
+        "Authorization": "Bearer " + session?.user?.accessToken
+      }
+    });
+    let result = await response.text();
+    alert(result);
+  }
+  var result = Object.entries(props.group.users);
+console.log(result.length);
   return (
     <>
       <div className={styles.container}>
@@ -23,10 +36,10 @@ export const Card = (props:any) => {
         <div className={styles.fio}>
           <h4>{props.group.name}</h4>
           <p>{props.group.description}</p>
-          <p>{faker.number.int(500)} учасников</p>
+          <p>{Object.entries(props.group.users).length} учасников</p>
         </div>
         <div className={styles.buttons}>
-          <Link href='/' className={styles.blueButton} ><AiOutlineUsergroupAdd size={16} /></Link>
+          <button className={styles.blueButton} onClick={JoinGroup} ><AiOutlineUsergroupAdd size={16} /></button>
         </div>
       </div>
     </>
