5d8e1f0e654c82b22e07d1e67931e84fa94106a2
VLola <valiklolanew@gmail.com>
2023-08-05 16:09:40 +0300

    changed messaging (remove dialog, fixed bugs)

--- src/components/messaging/dropDownItem/page.tsx
+++ src/components/messaging/dropDownItem/page.tsx
@@ -1,24 +1,27 @@
 import styles from './styles.module.scss'
-import { FC, useState } from "react";
-import Link from 'next/link';
+import { useState } from "react";
 import { HiDotsVertical } from 'react-icons/hi'
 
-export const DropDownItem: FC = () => {
+export function DropDownItem(props: any) {
   const [isOpen, setIsOpen] = useState(false);
+
+  const click = () =>{
+    props.removeDialog(props.id);
+  }
 
   return (
     <>
       <div className={styles.container}>
         <button type="button" onClick={() => setIsOpen((prev) => !prev)} onFocus={() => {if(isOpen)setIsOpen(true)}} onBlur={() => setIsOpen(false)}>
           <HiDotsVertical size={40} {...isOpen?{className:"bg-buttonBlue p-3 rounded-full fill-white"}:{className:"bg-buttonBlueOpacity p-3 rounded-full fill-buttonBlue"}}/>
+          
+          {isOpen &&
+            <div className={styles.dropMenu}>
+              <div className='text-componentText hover:text-buttonBlue'>View profile</div>
+              <div className='text-componentText hover:text-buttonRed' onClick={click}>Delete chat</div>
+            </div>
+          }
         </button>
-        
-        {isOpen &&
-          <div className={styles.dropMenu}>
-            <Link href='/'>View profile</Link>
-            <Link href='/'>Delete chat</Link>
-          </div>
-        }
       </div>
     </>
   )
