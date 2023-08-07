e9961f146f6c0d5aa7b75d9ffb4c8d00ac46fd24
VLola <valiklolanew@gmail.com>
2023-08-07 17:54:08 +0300

    refactoring messaging

--- src/app/messaging/page.tsx
+++ src/app/messaging/page.tsx
@@ -6,12 +6,8 @@ import React from 'react';
 import MainBlock from '@/components/messaging/mainBlock/page';
 import FooterBlock from '@/components/messaging/footerBlock/page';
 import { IUser } from '@/dto/sessionDto';
-import { HiMiniPencilSquare } from 'react-icons/hi2';
-import { IoMdClose } from 'react-icons/io';
-import { FaCircleUser } from 'react-icons/fa6';
-import { BsFillSendFill } from 'react-icons/bs';
-import { FaUserCircle } from 'react-icons/fa';
-import UserDialog from '@/components/messaging/userDialog/page';
+import { Dialogues } from '@/components/messaging/dialogues/page';
+import { NewMessage } from '@/components/messaging/newMessage/page';
 
 
 type MyProps = {
@@ -22,13 +18,7 @@ type MyState = {
   messages: [],
   dialogs: [],
   users: IUser[],
-  filterUsers: IUser[],
   user: IUser | null,
-  isOpen: boolean,
-  isOpenNewMessage: boolean,
-  message: string,
-  findUser: IUser | null,
-  search: string,
 };
 
 class Messaging extends React.Component<MyProps, MyState>{
@@ -42,23 +32,13 @@ class Messaging extends React.Component<MyProps, MyState>{
     messages: [],
     dialogs: [],
     users: [],
-    filterUsers: [],
     user: null,
-    isOpen: false,
-    isOpenNewMessage: false,
-    message: "",
-    findUser: null,
-    search: "",
   };
 
   constructor(props: any) {
     super(props);
     this.loadMessages = this.loadMessages.bind(this);
-    this.changeMessage = this.changeMessage.bind(this);
-    this.sendMessage = this.sendMessage.bind(this);
     this.click = this.click.bind(this);
-    this.select = this.select.bind(this);
-    this.changeSearch = this.changeSearch.bind(this);
     this.loadDialogs = this.loadDialogs.bind(this);
     this.loadUsers = this.loadUsers.bind(this);
     this.removeDialog = this.removeDialog.bind(this);
@@ -92,49 +72,6 @@ class Messaging extends React.Component<MyProps, MyState>{
     }
   }
 
-  select(findUser: IUser) {
-    if (findUser !== this.state.findUser) {
-      this.setState({ findUser: findUser });
-    }
-  }
-
-  changeMessage(event: any){
-    this.setState({message: event.target.value});
-  }
-  
-  changeSearch(event: any){
-    this.setState({search: event.target.value});
-    if(event.target.value === ""){
-      this.setState({filterUsers: this.state.users});
-    }
-    else{
-      let search = event.target.value.toLowerCase();
-      let users = this.state.users.filter(u=>u.firstName?.toLowerCase().includes(search) || u.lastName?.toLowerCase().includes(search) || u.email?.toLowerCase().includes(search) || u.phone?.toLowerCase().includes(search) || (u.firstName?.toLowerCase() + " " + u.lastName?.toLowerCase()).includes(search) || (u.lastName?.toLowerCase() + " " + u.firstName?.toLowerCase()).includes(search));
-      this.setState({filterUsers: users});
-    }
-  }
-
-  async sendMessage(){
-    if(this.state.findUser !== null){
-      const formData = new FormData();
-      formData.append("id", this.state.findUser.id);
-      formData.append("text", this.state.message);
-
-      const response = await fetch(process.env.NEXT_PUBLIC_STRAPI_API + "User/SendMessage", {
-        method: "POST",
-        headers: {
-          "Accept": "application/json",
-          "Authorization": "Bearer " + this.props.token
-        },
-        body: formData,
-      });
-  
-      this.setState({message: ""});
-      if (response.ok) {
-        this.loadDialogs();
-      }
-    }
-  }
 
   async removeDialog(id: string){
     const response = await fetch(process.env.NEXT_PUBLIC_STRAPI_API + "User/RemoveDialog?id=" + id, {
@@ -157,23 +94,8 @@ class Messaging extends React.Component<MyProps, MyState>{
         <div className={styles.container}>
           <div className={styles.centerContainer}>
             <div className={styles.leftContainer}>
-              <div>
-                <div className='m-5 flex justify-between items-center'>
-                  <h2>Активные чаты <span className={styles.chats}>{this.state.dialogs.length}</span></h2>
-                  <button onClick={() => this.setState({ isOpen: !this.state.isOpen, isOpenNewMessage: true })} onFocus={() => { if (this.state.isOpen) this.setState({ isOpen: true }) }} onBlur={() => this.setState({ isOpen: false })} >
-
-                    <HiMiniPencilSquare size={40} {...this.state.isOpen ? { className: "bg-buttonBlue p-3 rounded-full fill-white" } : { className: "bg-buttonBlueOpacity p-3 rounded-full fill-buttonBlue" }} />
-                  </button>
-                </div>
-                <hr className={styles.horizontalHr} />
-              </div>
-              <ul className={styles.users}>
-                {this.state.dialogs.map((dialog: any, index: any) =>
-                  <li key={index} onClick={() => this.click(dialog.user)} {...this.state.user?.id === dialog.user.id ? { className: "bg-slate-200 rounded-lg dark:bg-zinc-700" } : { className: "" }}>
-                    <UserDialog dialog={dialog}/>
-                  </li>
-                )}
-              </ul>
+              <NewMessage length={this.state.dialogs.length} users={this.state.users} loadDialogs={this.loadDialogs} token={this.props.token}/>
+              <Dialogues dialogs={this.state.dialogs} click={this.click} user={this.state.user}/>
             </div>
             <hr className={styles.verticalHr} />
             <div className={styles.rightContainer}>
@@ -189,45 +111,6 @@ class Messaging extends React.Component<MyProps, MyState>{
           </div>
         </div>
 
-        <div {...this.state.isOpenNewMessage ? { className: styles.newMessage + " visible z-50" } : { className: styles.newMessage + " invisible z-50" }}>
-          <div className={styles.newMessageContainer}>
-            <div className={styles.newMessageHeader}>
-              <h2>Новое сообщение</h2>
-              <button onClick={() => this.setState({ isOpenNewMessage: false })}>
-                <IoMdClose size={26} className={styles.buttonClose} />
-              </button>
-            </div>
-            <hr className={styles.horizontalHr} />
-            <div className={styles.newMessageContent}>
-              <div className="flex">
-                <span className={styles.iconSearch}>
-                  <FaCircleUser size={20} />
-                </span>
-                <input type="text" className={styles.inputSearch} placeholder="Введите имя" onChange={this.changeSearch} value={this.state.search} />
-              </div>
-              <ul className={styles.users + " h-2/4 overflow-auto"}>
-                {this.state.filterUsers.map((user: any, index: any) =>
-                  <li key={index} onClick={() => this.select(user)} {...this.state.findUser === user ? { className: "mx-2 bg-slate-200 rounded-lg dark:bg-zinc-700" } : { className: "mx-2" }}>
-                    <div className={styles.user}>
-                      {user.avatarUrl?(<img className={styles.userImage} src={user.avatarUrl}/>):(<FaUserCircle className={styles.userImage} />)}
-                      <div className={styles.userInfo}>
-                        <span className={styles.userName}>{user.firstName} {user.lastName}</span>
-                      </div>
-                    </div>
-                  </li>
-                )}
-              </ul>
-              <div className='flex justify-between'>
-                <textarea className={styles.textarea} onChange={this.changeMessage} value={this.state.message}></textarea>
-                <div className={styles.buttonContainer}>
-                  <button className={styles.button} onClick={this.sendMessage}>
-                    <BsFillSendFill className='fill-white'/>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </>
     );
   }
@@ -261,7 +144,6 @@ class Messaging extends React.Component<MyProps, MyState>{
     if (response.ok) {
       let result = await response.json();
       this.setState({ users: result });
-      this.setState({ filterUsers: result });
     }
   }
 }
---
+++ src/components/messaging/newMessage/page.tsx
@@ -0,0 +1,111 @@
+import styles from './styles.module.scss'
+import { HiMiniPencilSquare } from 'react-icons/hi2';
+import { IoMdClose } from 'react-icons/io';
+import { FaCircleUser } from 'react-icons/fa6';
+import { BsFillSendFill } from 'react-icons/bs';
+import { FaUserCircle } from 'react-icons/fa';
+import { useState } from "react";
+import { IUser } from '@/dto/sessionDto';
+
+export function NewMessage(props: any) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenNewMessage, setIsOpenNewMessage] = useState(false);
+  const [search, setSearch] = useState("");
+  const [message, setMessage] = useState("");
+  const [filterUsers, setFilterUsers] = useState([]);
+  const [findUser, setFindUser] = useState(null);
+
+  const changeSearch = (event: any) => {
+    setSearch(event.target.value);
+    if (event.target.value === "") {
+      setFilterUsers(props.users);
+    }
+    else {
+      let search = event.target.value.toLowerCase();
+      let users = props.users.filter((u: any) => u.firstName?.toLowerCase().includes(search) || u.lastName?.toLowerCase().includes(search) || u.email?.toLowerCase().includes(search) || u.phone?.toLowerCase().includes(search) || (u.firstName?.toLowerCase() + " " + u.lastName?.toLowerCase()).includes(search) || (u.lastName?.toLowerCase() + " " + u.firstName?.toLowerCase()).includes(search));
+      setFilterUsers(users);
+    }
+  }
+
+  const select =(find: any)=> {
+    if (find !== findUser) {
+      setFindUser(find);
+    }
+  }
+
+
+  const sendMessage = async (mess:string, find: IUser) => {
+    if (findUser !== null) {
+      const formData = new FormData();
+      formData.append("id", find.id);
+      formData.append("text", mess);
+
+      const response = await fetch(process.env.NEXT_PUBLIC_STRAPI_API + "User/SendMessage", {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Authorization": "Bearer " + props.token
+        },
+        body: formData,
+      });
+
+      setMessage("");
+      if (response.ok) {
+        props.loadDialogs();
+      }
+    }
+  }
+
+  return (
+    <div>
+      <div className='m-5 flex justify-between items-center'>
+        <h2>Активные чаты <span className={styles.chats}>{props.length}</span></h2>
+        <button onClick={() => { setIsOpen(!isOpen); setIsOpenNewMessage(true); }} onFocus={() => { if (isOpen) setIsOpen(true) }} onBlur={() => setIsOpen(false)} >
+
+          <HiMiniPencilSquare size={40} {...isOpen ? { className: "bg-buttonBlue p-3 rounded-full fill-white" } : { className: "bg-buttonBlueOpacity p-3 rounded-full fill-buttonBlue" }} />
+        </button>
+      </div>
+      <hr className={styles.horizontalHr} />
+
+      <div {...isOpenNewMessage ? { className: styles.newMessage + " visible z-50" } : { className: styles.newMessage + " invisible z-50" }}>
+        <div className={styles.newMessageContainer}>
+          <div className={styles.newMessageHeader}>
+            <h2>Новое сообщение</h2>
+            <button onClick={() => setIsOpenNewMessage(false)}>
+              <IoMdClose size={26} className={styles.buttonClose} />
+            </button>
+          </div>
+          <hr className={styles.horizontalHr} />
+          <div className={styles.newMessageContent}>
+            <div className="flex">
+              <span className={styles.iconSearch}>
+                <FaCircleUser size={20} />
+              </span>
+              <input type="text" className={styles.inputSearch} placeholder="Введите имя" onChange={(e) => { changeSearch(e) }} value={search} />
+            </div>
+            <ul className={styles.users + " h-2/4 overflow-auto"}>
+              {filterUsers.map((user: any, index: any) =>
+                <li key={index} onClick={() => select(user)} {...findUser === user ? { className: "mx-2 bg-slate-200 rounded-lg dark:bg-zinc-700" } : { className: "mx-2" }}>
+                  <div className={styles.user}>
+                    {user.avatarUrl ? (<img className={styles.userImage} src={user.avatarUrl} />) : (<FaUserCircle className={styles.userImage} />)}
+                    <div className={styles.userInfo}>
+                      <span className={styles.userName}>{user.firstName} {user.lastName}</span>
+                    </div>
+                  </div>
+                </li>
+              )}
+            </ul>
+            <div className='flex justify-between'>
+              <textarea className={styles.textarea} onChange={(e)=>{setMessage(e.target.value)}} value={message}></textarea>
+              <div className={styles.buttonContainer}>
+                <button className={styles.button} onClick={()=>{if(findUser !== null)sendMessage(message, findUser);}}>
+                  <BsFillSendFill className='fill-white' />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
