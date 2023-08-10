39ac1ba49513d2c1465d74ebc7710acb3782122a
VLola <valiklolanew@gmail.com>
2023-08-10 18:18:35 +0300

    added component photos

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -9,14 +9,14 @@ import { Stories } from '@/components/main/stories/stories';
 import { GroupsCard } from "@/components/userProfile/groupsCard/groupsCard";
 import { useState } from 'react';
 import styles from './styles.module.scss';
+import { Photos } from "@/components/photos/page";
 
 export default function Home(props: any) {
 
   const [component, setComponent] = useState("");
   const ChangeComponent = () => {
     if (component === "groups") return (<GroupsCard />)
-
-
+    else if (component === "photos") return (<Photos />)
     else return (
       <>
         <div>
