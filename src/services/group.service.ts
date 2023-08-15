9f102493b99abfd128419bb67a0566405dcba5b9
loboda.rom@gmail.com <loboda.rom@gmail.com>
2023-08-15 19:39:14 +0300

    groupService,groupPicture

---
+++ src/services/group.service.ts
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { getSession } from 'next-auth/react';
+
+export class GroupService {
+    static async getGroups() {
+        const session = await getSession();
+        return await axios.get(process.env.NEXT_PUBLIC_STRAPI_API + "Group/GetGroups", {
+            headers: {
+                "Accept": "application/json",
+                "Authorization": "Bearer " + session?.user.accessToken,
+            },
+        }).then(response => response.data);
+    }
+    static async addGroup(formData: FormData) {
+        const session = await getSession();
+        return await axios.post(process.env.NEXT_PUBLIC_STRAPI_API + "Group/AddGroup", formData, {
+            headers: {
+                "Accept": "application/json",
+                "Authorization": "Bearer " + session?.user.accessToken,
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+    }
+    static async joinGroup(id:string) {
+        const session = await getSession();
+        return await axios.post(process.env.NEXT_PUBLIC_STRAPI_API + "Group/JoinGroup", { id: id }, {
+            headers: {
+                "Accept": "application/json",
+                "Authorization": "Bearer " + session?.user.accessToken,
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+    static async leaveGroup(id: string) {
+        const session = await getSession();
+        return await axios.delete(process.env.NEXT_PUBLIC_STRAPI_API + "Group/LeaveGroup?id=" + id, {
+          headers: {
+            "Accept": "application/json",
+            "Authorization": "Bearer " + session?.user.accessToken,     
+          },
+        }).then(response => response.data)
+      }
+
+
+
+
+
+
+
+
+
+}
