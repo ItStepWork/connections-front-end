8958ff55d2ef80de3e17268b37787b0944657568
Illia <Illia@DESKTOP-TG4JUT1>
2023-08-25 15:44:20 +0200

    +add service

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -4,10 +4,55 @@ import { getSession } from 'next-auth/react';
   export class PostService {
 
     async createPost(){
-        const post: Post = {
-          Text: model.Text,
-          ImageUrl: model.ImageUrl,
-          VideoUrl: model.VideoUrl
-        };
+      const session = await getSession();
+      return await axios.get(process.env.NEXT_PUBLIC_STRAPI_API + `Post/GetPost`, {
+          headers: {
+              "Accept": "application/json",
+              "Authorization": "Bearer " + session?.user.accessToken,
+          },
+      }).then(response => response.data);
+  }
+  async getPostById(postId: string) {
+    const session = await getSession();
+        return await axios.get(process.env.NEXT_PUBLIC_STRAPI_API + `Post/GetPost?postId=${postId}`, {
+            headers: {
+                "Accept": "application/json",
+                "Authorization": "Bearer " + session?.user.accessToken,
+            },
+        }).then(response => response.data);
+  }
+ async addComment(senderId: string, userId: string, postId: string, text: string) {
+    
+    const session = await getSession();
+    const requestData = {
+      senderId: senderId,
+      userId: userId,
+      postId: postId,
+      text: text
+  };
+    return await axios.post(process.env.NEXT_PUBLIC_STRAPI_API + "Post/AddComment", requestData, {
+        headers: {
+            "Accept": "application/json",
+            "Authorization": "Bearer " + session?.user.accessToken,
+        },
+    }).then(response => response.data);
+    };
+
+    
+  
+
+  async likePost(senderId: string, userId: string, postId: string){
+    const session = await getSession();
+    const requestData = {
+      senderId: senderId,
+      userId: userId,
+      postId: postId,
+  };
+    return await axios.post(process.env.NEXT_PUBLIC_STRAPI_API + "Post/LikePost", requestData, {
+        headers: {
+            "Accept": "application/json",
+            "Authorization": "Bearer " + session?.user.accessToken,
+        },
+    }).then(response => response.data);
   }
 }
