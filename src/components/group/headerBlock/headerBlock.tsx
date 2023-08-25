c4538ef4eefbf1c86560224e7f194e0b55fbdcd6
loboda.rom@gmail.com <loboda.rom@gmail.com>
2023-08-25 19:49:56 +0300

    addRequest,Members,AcceptRequestsToGroup

--- src/components/group/headerBlock/headerBlock.tsx
+++ src/components/group/headerBlock/headerBlock.tsx
@@ -77,7 +77,7 @@ export function HeaderBlock(props: any) {
                   <h2>{props.group.name}</h2>
                   <span><BsFillPatchCheckFill size={18} /></span>
                 </div>
-                <p> {props.group.audience} группа - {Object.entries(props.users).length} участников</p>
+                <p> {props.group.audience} группа - {Object.entries(props.members).length} участников</p>
                 <div className={styles.description}>
                   <p className="word-break: break-all"> {props.group.description}</p>
                 </div>
@@ -96,17 +96,17 @@ export function HeaderBlock(props: any) {
           </div>
           <div className={styles.membersContainer}>
             <div className={styles.members}>
-              {props.users.map((user: any, index: any) => {
+              {props.members.map((user: any, index: any) => {
                 if (index < 12) {
                   if (user.avatarUrl !== undefined && user.avatarUrl !== null && user.avatarUrl !== "") return (<div key={index} className="w-4"><img className={styles.memberIco} src={user.avatarUrl}></img></div>)
                   else return (<div key={index} className="w-4"><img className={styles.memberIco} src={faker.image.avatar()}></img></div>)
                 }
               })}
-              <div className="w-4"><div className={styles.membersDiv}>{Object.entries(props.users).length}</div></div>
+              <div className="w-4"><div className={styles.membersDiv}>{Object.entries(props.members).length}</div></div>
             </div>
             <div className={styles.membersNames}>
               <p>
-                {props.users.map((user: any, index: any) => {
+                {props.members.map((user: any, index: any) => {
                   if (index < 6) {
                     if (user.firstName || user.lastName) return (<a key={index}>{user.firstName + " " + user.lastName}, </a>)
                     else return (<a key={index}>no name,</a>)
@@ -123,12 +123,16 @@ export function HeaderBlock(props: any) {
             <button {...component === "about" ? { className: `${styles.linkUnderline}` } : { className: `${styles.link}` }} onClick={() => { setComponent("about"); props.setComponent("about") }}>О группе</button>
           </div>
           <div {...component === "members" ? { className: `${styles.counterLink}` } : { className: "flex items-center" }} >
-            <button {...component === "members" ? { className: `${styles.linkUnderline}` } : { className: `${styles.link}` }} onClick={() => { setComponent("members"); props.setComponent("connections"); }}>Участники</button>
-            <div className={styles.counter}>{Object.entries(props.users).length}</div>
+            <button {...component === "members" ? { className: `${styles.linkUnderline}` } : { className: `${styles.link}` }} onClick={() => { setComponent("members"); props.setComponent("members"); }}>Участники</button>
+            <div className={styles.counter}>{Object.entries(props.members).length}</div>
           </div>
-          <div {...component === "requests" ? { className: `${styles.counterLink}` } : { className: "" }} >
-            <button {...component === "requests" ? { className: `${styles.linkUnderline}` } : { className: `${styles.link}` }} onClick={() => { setComponent("requests") }}>Запросы</button>
-          </div>
+          {ifAdmin() &&
+            <div {...component === "requests" ? { className: `${styles.counterLink}` } : { className: "flex items-center" }} >
+              <button {...component === "requests" ? { className: `${styles.linkUnderline}` } : { className: `${styles.link}` }} onClick={() => { setComponent("requests"); props.setComponent("requests"); }}>Запросы</button>
+              <div className={styles.counter}>{Object.entries(props.usersRequests).length}</div>
+            </div>
+          }
+
           <div {...component === "media" ? { className: `${styles.counterLink}` } : { className: "" }} >
             <button {...component === "media" ? { className: `${styles.linkUnderline}` } : { className: `${styles.link}` }} onClick={() => { setComponent("media") }}>Медиа</button>
           </div>
