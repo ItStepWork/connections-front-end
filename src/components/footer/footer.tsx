a2e79bfb6eb8337834efb26f761bca1c90be8594
Tibilex <45212175+Tibilex@users.noreply.github.com>
2023-08-28 17:28:59 +0300

    Fixed: footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,17 +3,19 @@ const Footer = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.links}>
-          <ul className={styles.list}>
-            <li><a href="">О нас</a></li>
-            <li><a href="">Помощь</a></li>
-            <li><a href="">Конфиденциальность и условия</a></li>
-            <li><a href="">Партнерство</a></li>
-            <li><a href="">Защита</a></li>
-          </ul>
+        <div className={styles.contentContainer}>
+          <div className={styles.links}>
+            <ul className={styles.list}>
+              <li><a href="">О нас</a></li>
+              <li><a href="">Помощь</a></li>
+              <li><a href="">Конфиденциальность и условия</a></li>
+              <li><a href="">Партнерство</a></li>
+              <li><a href="">Защита</a></li>
+            </ul>
+          </div>
           <div className={styles.copyright}>
             <p>©2023 <a className={styles.brandLink} href="">Connections</a> Все права защищены.</p>
-            </div>
+          </div>
         </div>
       </div>
     </>
