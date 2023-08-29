1264e917c4f87aa567d080194c4e39cc4aa70ca2
VLola <valiklolanew@gmail.com>
2023-08-29 18:42:26 +0300

    changed notification service

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,13 +1,15 @@
 export const SendNotification = (name: string, text: string) => {
-    Notification.requestPermission().then((perm)=>{
-        if(perm == "granted"){
-            new Notification(name, {
-                body: text,
-                icon: "/public/icon-192x192.png",
-            });
+    navigator.serviceWorker.getRegistration().then((reg:any) => {
+        console.log(reg)
+        const options = {
+            body: text,
+            icon: '/public/icon-192x192.png',
+            vibrate: [100, 50, 100],
+            data: {
+                dateOfArrival: Date.now(),
+                primaryKey: 0,
+            },
         }
-        else{
-            console.log("Permission denied");
-        }
+        reg.showNotification(name, options);
     });
 }
