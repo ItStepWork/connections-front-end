9084e40615b629a0bbfed57436e1e2d545207412
VLola <valiklolanew@gmail.com>
2023-08-31 00:03:02 +0300

    changed worker

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const withPWA = require('next-pwa')({
 })
 
 module.exports = withPWA({
+	reactStrictMode: true,
 	images: {
 		domains: [
 			'loremflickr.com',
--- worker/index.js
+++ worker/index.js
@@ -1,12 +1,29 @@
 'use strict'
 
-import Firebase from '/src/services/firebase.service';
+import Firebase from '../src/services/firebase.service';
 import { ref, onChildChanged } from 'firebase/database'
 
-onChildChanged(ref(Firebase(), `Messages`), (data) => {
-  registration.showNotification("Valik", {
-    body: "ky ky",
-    icon: '/icons/android-chrome-192x192.png'
-  })
-  console.log(data);
-});
+let i = 0;
+
+function sayHello(name) {
+  console.log(`${name} ${i++}`)
+}
+
+const load = async () => {
+  try {
+    onChildChanged(ref(Firebase(), `Messages`), (data) => {
+      registration.showNotification("Valik", {
+        body: "ky ky",
+        icon: '/icons/android-chrome-192x192.png'
+      })
+      console.log(data);
+    });
+  }
+  catch (error) {
+    console.log("error:");
+    console.log(error);
+  }
+}
+
+setInterval(sayHello, 10000, "Worker")
+load();
