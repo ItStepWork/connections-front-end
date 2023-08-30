8468534ce869a429b82aa875d1ae357f17f40efb
VLola <valiklolanew@gmail.com>
2023-08-30 15:39:41 +0300

    changed notification

--- src/services/notification.service.js
+++ src/services/notification.service.js
@@ -1,4 +1,4 @@
-export const SendNotification = (name, text) => {
+export const SendNotification = async (name, text) => {
     const options = {
         body: text,
         icon: '/public/icon-192x192.png',
@@ -11,7 +11,12 @@ export const SendNotification = (name, text) => {
     Notification.requestPermission(status=>{
         console.log("status:", status);
     })
-    navigator.serviceWorker.getRegistration().then(reg=>{
-        reg.showNotification(name, options);
+    navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification(name, options);
     });
+    // const reg = await navigator.serviceWorker.getRegistration();
+    //  reg.showNotification(name, options);
+    // navigator.serviceWorker.getRegistration().then(reg=>{
+    //     reg.showNotification(name, options);
+    // });
 }
