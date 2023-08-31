95b726062a029b76a25f5e237cb426d37f27d45c
VLola <valiklolanew@gmail.com>
2023-08-31 17:40:27 +0300

    changed styles selected photo

--- src/components/gallery/selectedPhoto/page.tsx
+++ src/components/gallery/selectedPhoto/page.tsx
@@ -17,8 +17,10 @@ export default function SelectedPhoto(props: any) {
               <div className=' w-10/12 h-full block overflow-hidden'>
 
                 <div className='flex p-3 h-full w-full flex-col flex-wrap justify-center items-center bg-white dark:bg-dark_background'>
-                  <img className='object-contain h-44 md:h-64 lg:h-96 w-auto' src={props.photos[props.selectedIndex].url} />
-                  <InfoPhoto photo={props.photos[props.selectedIndex]} myId={props.myId} userId={props.userId} get={props.get} />
+                  <img className='object-contain h-44 md:h-64 lg:h-96 w-auto max-w-[240px] md:max-w-[360px]' src={props.photos[props.selectedIndex].url} />
+                  <div className='text-sm flex flex-col my-3 max-h-64 md:max-h-96 lg:max-h-full max-w-[240px] md:max-w-[360px] overflow-y-auto'>
+                    <InfoPhoto photo={props.photos[props.selectedIndex]} myId={props.myId} userId={props.userId} get={props.get} />
+                  </div>
                 </div>
               </div>
             ) : (<></>)}
