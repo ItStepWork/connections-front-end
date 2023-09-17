f7b9968731c1e59fead416f0b03c33c36ee56630
VLola <valiklolanew@gmail.com>
2023-09-17 16:34:44 +0300

    changed messaging page

--- app/[lang]/components/messaging/main/messaging.tsx
+++ app/[lang]/components/messaging/main/messaging.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { getSession } from 'next-auth/react';
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { HiMiniPencilSquare } from 'react-icons/hi2';
 import { IUser } from '../../../../../dto/sessionDto';
 import { MessagingService } from '../../../../../services/messaging.service';
@@ -15,150 +15,97 @@ import NewMessage from '../newMessage/page';
 import Window from '../window/page';
 import styles from './styles.module.scss';
 
-type MyState = {
-  messages: [],
-  dialogs: [],
-  users: IUser[],
-  user: IUser | null,
-  isOpen: boolean,
-  isOpenDialogs: boolean,
-  socket: WebSocket | null,
-  intervalId: string | number | NodeJS.Timeout | undefined,
-};
+export default function Messaging({ local, session }: { local: any, session: any }, props: any) {
 
-export default class Messaging extends React.Component{
+  const [user, setUser] = useState<any>(null);
+  const [users, setUsers] = useState<any[]>([]);
+  const [dialogs, setDialogs] = useState<any[]>([]);
+  const [messages, setMessages] = useState<any[]>([]);
+  const [isOpenDialogs, setIsOpenDialogs] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  state: MyState = {
-    messages: [],
-    dialogs: [],
-    users: [],
-    user: null,
-    isOpen: false,
-    isOpenDialogs: false,
-    socket: null,
-    intervalId: undefined,
-  };
-
-  constructor(props: any) {
-    super(props);
-    this.loadMessages = this.loadMessages.bind(this);
-    this.click = this.click.bind(this);
-    this.subscribe = this.subscribe.bind(this);
-    this.loadDialogs = this.loadDialogs.bind(this);
-    this.loadUsers = this.loadUsers.bind(this);
-    this.removeDialog = this.removeDialog.bind(this);
-    this.setIsOpen = this.setIsOpen.bind(this);
-    this.setIsOpenDialogs = this.setIsOpenDialogs.bind(this);
-  }
-
-  async componentDidMount() {
-    await this.subscribe();
-    await this.loadDialogs();
-    await this.loadUsers();
-  }
-
-  componentWillUnmount(): void {
-    this.state.socket?.close();
-    clearInterval(this.state.intervalId);
-  }
-
-  componentWillUpdate(nextProps: Readonly<{}>, nextState: Readonly<MyState>, nextContext: any): void {
-    if(this.state.socket !== nextState.socket) {
-      let socket = this.state.socket;
-      setTimeout(()=>{socket?.close();}, 1000)
+  const click = (value: IUser) => {
+    if (value !== user) {
+      const u = value;
+      setUser(value);
+      loadMessages(value.id);
+      if (isOpenDialogs) setIsOpenDialogs(false);
     }
-    if(this.state.intervalId !== nextState.intervalId) clearInterval(this.state.intervalId);
   }
 
-  async loadMessages(id: string) {
+  const loadMessages = async (id: string) => {
     const result = await MessagingService.getMessages(id);
-    this.setState({ messages: result });
+    setMessages(result)
   }
 
-  click(user: IUser) {
-    if (user !== this.state.user) {
-      this.setState({ user: user });
-      this.loadMessages(user.id);
-      if (this.state.isOpenDialogs) this.setState({ isOpenDialogs: false });
-    }
-  }
-
-  async removeDialog(id: string) {
+  const removeDialog = async (id: string) => {
     await MessagingService.removeDialog(id);
-    this.loadDialogs();
-    this.setState({ messages: [], user: null });
+    loadDialogs();
+    setMessages([]);
+    setUser(null);
   }
 
-  setIsOpen(isOpen: boolean) {
-    this.setState({ isOpen: isOpen });
-  }
-
-  setIsOpenDialogs(isOpenDialogs: boolean) {
-    this.setState({ isOpenDialogs: isOpenDialogs });
-  }
-
-  render() {
-    return (
-      <div className={styles.container}>
-
-        <div className='absolute flex mt-16 lg:invisible'>
-          <DropDownDialogues dialogs={this.state.dialogs} click={this.click} user={this.state.user} isOpen={this.state.isOpenDialogs} setIsOpen={this.setIsOpenDialogs} /><h2 className='my-1 mx-2'>Чаты</h2>
-        </div>
-        <div className={styles.centerContainer}>
-          <div className={styles.leftContainer}>
-            <div className='m-3 flex justify-between items-center'>
-              <h2>Активные чаты <span className={styles.chats}>{this.state.dialogs.length}</span></h2>
-              <button {...this.state.isOpen ? { className: styles.buttonOpen } : { className: styles.buttonClose }} onClick={() => this.setIsOpen(true)}>
-                <HiMiniPencilSquare />
-              </button>
-              <Window name="Новое сообщение" isOpen={this.state.isOpen} setIsOpen={this.setIsOpen}>
-                <NewMessage users={this.state.users} />
-              </Window>
-            </div>
-            <hr className={styles.horizontalHr} />
-            <div className='invisible h-0 lg:visible lg:h-auto overflow-y-auto'>
-              <Dialogues dialogs={this.state.dialogs} click={this.click} user={this.state.user} />
-            </div>
-          </div>
-          <hr className={styles.verticalHr} />
-          {this.state.user ? (
-            <div className={styles.rightContainer}>
-              <HeaderBlock user={this.state.user} removeDialog={this.removeDialog} />
-              <MainBlock messages={this.state.messages} user={this.state.user}/>
-              <hr className={styles.horizontalHr} />
-              <FooterBlock friendId={this.state.user.id}/>
-            </div>
-          ) : (<div className={styles.rightContainer}></div>)}
-        </div>
-      </div>
-    );
-  }
-
-  async loadDialogs() {
+  const loadDialogs = async () => {
     let result = await MessagingService.getDialogs();
-    this.setState({ dialogs: result });
+    setDialogs(result);
   }
 
-  async loadUsers() {
+  const loadUsers = async () => {
     let result = await UserService.getUsers();
-    this.setState({ users: result });
+    setUsers(result);
   }
 
-  async subscribe() {
-    let session = await getSession();
-    if (session != null) {
-      let socket = new WebSocket(process.env.NEXT_PUBLIC_SUBSCRIPTION_API + `Subscription/SubscribeToMessagesUpdates`, ["client", session.user.accessToken]);
-      socket.addEventListener('message', (event) => {
-        this.loadDialogs();
-        if (this.state.user !== null) {
-          this.loadMessages(this.state.user.id);
-        }
-      });
-      let intervalId = setInterval(() => {
-        if(socket.OPEN)socket.send("ping");
-        else clearInterval(intervalId);
-      }, 30000);
-      this.setState({socket: socket, intervalId: intervalId});
-    }
-  }
+  useEffect(() => {
+    loadDialogs();
+    loadUsers();
+    let socket = new WebSocket(process.env.NEXT_PUBLIC_SUBSCRIPTION_API + `Subscription/SubscribeToMessagesUpdates`, ["client", session.user.accessToken]);
+    socket.addEventListener('message', (event) => {
+      loadDialogs();
+      if (user !== null) {
+        loadMessages(user.id);
+      }
+    });
+    let intervalId = setInterval(() => {
+      if (socket.OPEN) socket.send("ping");
+      else clearInterval(intervalId);
+    }, 30000);
+    return () => {
+      setInterval(() => { if (socket.OPEN) socket.close(); }, 1000)
+      clearInterval(intervalId);
+    };
+  }, [user]);
+
+  return (
+    <div className={styles.container}>
+      <div className='absolute flex mt-16 lg:invisible'>
+        <DropDownDialogues dialogs={dialogs} click={click} user={user} isOpen={isOpenDialogs} setIsOpen={setIsOpenDialogs} /><h2 className='my-1 mx-2'>Чаты</h2>
+      </div>
+      <div className={styles.centerContainer}>
+        <div className={styles.leftContainer}>
+          <div className='m-3 flex justify-between items-center'>
+            <h2>Активные чаты <span className={styles.chats}>{dialogs.length}</span></h2>
+            <button {...isOpen ? { className: styles.buttonOpen } : { className: styles.buttonClose }} onClick={() => setIsOpen(true)}>
+              <HiMiniPencilSquare />
+            </button>
+            <Window name="Новое сообщение" isOpen={isOpen} setIsOpen={setIsOpen}>
+              <NewMessage users={users} />
+            </Window>
+          </div>
+          <hr className={styles.horizontalHr} />
+          <div className='invisible h-0 lg:visible lg:h-auto overflow-y-auto'>
+            <Dialogues dialogs={dialogs} click={click} user={user} />
+          </div>
+        </div>
+        <hr className={styles.verticalHr} />
+        {user ? (
+          <div className={styles.rightContainer}>
+            <HeaderBlock user={user} removeDialog={removeDialog} />
+            <MainBlock messages={messages} user={user} />
+            <hr className={styles.horizontalHr} />
+            <FooterBlock friendId={user.id} />
+          </div>
+        ) : (<div className={styles.rightContainer}></div>)}
+      </div>
+    </div>
+  )
 }
--- app/[lang]/messaging/page.tsx
+++ app/[lang]/messaging/page.tsx
@@ -1,5 +1,11 @@
+import { authConfig } from "../../../configs/auth";
+import { Locale } from "../../../i18n.config";
+import { getDictionary } from "../../../locale-dictionary";
 import Messaging from "../components/messaging/main/messaging";
+import { getServerSession } from "next-auth";
 
-export default async function MessagingPage(){
-  return (<Messaging />)
+export default async function MessagingPage({ params: { lang }}: { params: { lang: Locale }}, props : any){
+  const localDictionary = await getDictionary(lang)
+  const session = await getServerSession(authConfig);
+  return (<Messaging local={localDictionary}  session={session}/>)
 };
